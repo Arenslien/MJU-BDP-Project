77ae738caf4887d93c34dc2631e087df4865f8bf
Arenslien <arenslien@gmail.com>
2023-12-10 15:22:37 +0000

    edit

--- spark/paperAbstractAnalysis.py
+++ spark/paperAbstractAnalysis.py
@@ -1,6 +1,8 @@
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import split, explode, col, desc, row_number
 from pyspark.sql.window import Window
+from pyspark.sql import Row
+from pyspark.sql.types import StructField, StructType, StringType, LongType
 from urllib.parse import urlparse
 import os
 
@@ -9,13 +11,20 @@ def get_all_path(spark, path):
 	hadoop = spark._jvm.org.apache.hadoop
 	fs = hadoop.fs.FileSystem
 	conf = hadoop.conf.Configuration()
-	all_paths = hadoop.fs.Path(path)
-	dir_path = [str(f.getPath()) for f in fs.get(conf).listStatus(all_paths)]
+	input_path = hadoop.fs.Path(path)
+	year_paths = [str(f.getPath()) for f in fs.get(conf).listStatus(input_path)]
+	csv_paths = []
 	
-	parsed_path = ["hdfs://" + str(urlparse(f).path) + "/*/*.csv" for f in dir_path]
-	
-	return parsed_path
+	for y_path in year_paths:
+		year_path = hadoop.fs.Path(y_path)
+		file_paths = [str(file.getPath()) for file in fs.get(conf).listStatus(year_path)]
 
+		for f_path in file_paths:
+			if f_path.endswith(".csv"):
+				csv_path = "hdfs://" + str(urlparse(f_path).path)
+				csv_paths.append(csv_path)
+
+	return csv_paths
 
 if __name__=="__main__":
 
@@ -23,42 +32,58 @@ if __name__=="__main__":
 	spark = SparkSession.builder.appName("Yearly Paper Analysis").getOrCreate()
 	
 	# 0. directory setting
-	path = "hdfs:///user/maria_dev/arxiv-data/2015/"
-	get_path = get_all_path(spark, path)
-	print(get_path)
+	path = "hdfs:///user/maria_dev/archive_store/stop_word/"
+	csv_paths = get_all_path(spark, path)
+	print(csv_paths)
 	
-	#for dir in get_path:
+	all_years = []
+	all_year_publication = []
 	
-	# 1. Load Data	
-	cs_2021_full_df = spark.read.option("header", "true") \
-			.option("multiLine", "true") \
-			.option("escape", ",") \
-			.option("escape", '"') \
-			.csv("hdfs:///user/maria_dev/arxiv-data/2021/arxiv_CS_2021_full.csv")
-	cs_2021_full_df = cs_2021_full_df.limit(30)
+	publicationCountSchema = StructType([
+		StructField("Year", StringType(), nullable=False),
+		StructField("count", LongType(), nullable=False)
+	])
+	publication_count_df = spark.createDataFrame(spark.sparkContext.emptyRDD(), schema=publicationCountSchema)
 
-	# 2. Yearly(+ Monthly) Abstract Keyword Analysis
-	
-	# 2.1 Split Abstract & Word Count
-	splited_df = cs_2021_full_df.withColumn("Words", split(col("Abstract\r"), " ")) \
-			.select("Month", explode("Words").alias("Word"))
-	
-	# 2.2 Word Count
-	word_count_df = splited_df.groupBy("Month", "Word") \
+	for csv_path in csv_paths:
+		# 1. Load Data	
+		csv_df = spark.read.option("header", "true") \
+				.option("multiLine", "true") \
+				.option("escape", ",") \
+				.option("escape", '"') \
+				.csv(csv_path)
+		csv_df = csv_df.limit(30)
+
+		# 2. Add Yearly Publication Counting
+		grouped_df = csv_df.groupBy("Year").count()
+		publication_count_df = publication_count_df.union(grouped_df)
+
+		# 3. Yearly(+ Monthly) Abstract Keyword Analysis
+		
+		# 3.1 Split Abstract & Word Count
+		splited_df = csv_df.withColumn("Words", split(col("Abstract"), " ")) \
+				.select("Year", explode("Words").alias("Word"))
+		
+		# 3.2 Word Count
+		word_count_df = splited_df.groupBy("Year", "Word") \
 			.count()
 	
-	# 2.3 Sorting  Count --> Desc
-	sorted_df = word_count_df.orderBy("Month", desc("count"))
-	sorted_df.show()
+		# 3.3 Sorting  Count --> Desc
+		sorted_df = word_count_df.orderBy("Year", desc("count"))
+		sorted_df.show()
 	
-	# 2.4 Saving top K keyword per Month 
-	months = sorted_df.select("Month").distinct().collect()
+		# 3.4 Saving top K keyword per Month 
+		years = sorted_df.select("Year").distinct().collect()
 	
-	K = 30
-	for row in months:
-		top_K_keyword = sorted_df.where(col("Month") == row.Month).limit(30)
-		top_K_keyword.show(10)
-		save_dir = "hdfs:///user/maria_dev/arxiv-analysis-result/2021/abstract-keyword-" + row.Month
-		top_K_keyword.write.csv(save_dir)
+		K = 30
+		for row in years:
+			top_K_keyword = sorted_df.where(col("Year") == row.Year).limit(30)
+			top_K_keyword.show(10)
+			save_dir = "hdfs:///user/maria_dev/archive_store/abstract-keyword-" + row.Year
+			top_K_keyword.write.csv(save_dir)
 	
+	# 2.1 Save Yearly Publication Count
+	save_dir2 = "hdfs:///user/maria_dev/archive_store/Yearly-Publication-Count"
+	publication_count_df.write.csv(save_dir2)
+
 
--- spark/runPaperAnalysis.sh
+++ spark/runPaperAnalysis.sh
@@ -5,11 +5,8 @@ export LC_ALL=en_US.UTF-8
 export PYSPARK_PYTHON="/bin/python3.6"
 
 spark-submit --conf spark.yarn.appMasterEnv.PYSPARK_PYTHON=/bin/python3.6 \
+	stop_words.py
+
+spark-submit --conf spark.yarn.appMasterEnv.PYSPARK_PYTHON=/bin/python3.6 \
 	paperAbstractAnalysis.py
-
-spark-submit --conf spark.yarn.appMasterEnv.PYSPARK_PYTHON=/bin/python3.6 \
-	paperTitleAnalysis.py
-
-spark-submit --conf spark.yarn.appMasterEnv.PYSPARK_PYTHON=/bin/python3.6 \
-	paperPublicationCount.py
 
