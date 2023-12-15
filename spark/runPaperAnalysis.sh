e398499c9087e9db5e737b83ece060f32d2b9e0f
Arenslien <arenslien@gmail.com>
2023-12-15 14:09:05 +0000

    final paper Analysis code & visualization code & auto

--- spark/runPaperAnalysis.sh
+++ spark/runPaperAnalysis.sh
@@ -10,3 +10,15 @@ export PYSPARK_PYTHON="/bin/python3.6"
 spark-submit --conf spark.yarn.appMasterEnv.PYSPARK_PYTHON=/bin/python3.6 \
 	paperAbstractAnalysis.py
 
+mkdir ../analysis_result
+hdfs dfs -copyToLocal /user/maria_dev/archive_store/abstract-keyword ../analysis_result/
+hdfs dfs -copyToLocal /user/maria_dev/archive_store/Yearly-Publication-Count/ ../analysis_result/
+
+bash pipInstall.sh
+python3 visulization.py
+
+hdfs dfs -mkdir /user/maria_dev/archive_store/analysis_result
+hdfs dfs -copyFromLocal ./all_keyword_wordcloud.png archive_store/analysis_result/
+hdfs dfs -copyFromLocal ./yearly_publication_count.png archive_store/analysis_result/
+hdfs dfs -copyFromLocal ./Paper_Analysis_Result_Report.docx archive_store/analysis_result/
+
